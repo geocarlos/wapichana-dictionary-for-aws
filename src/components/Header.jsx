bf563f8df1593548f0a174281114f30f28482ca3
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-06-21 19:08:53 -0300

    Fix some data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../assets/logo.jpg';
 
 const Header = () => (
 	<header>
 		<div className="logo">
-			<figure></figure>
+			<figure>
+				<img width="200" height="100" src={logo} alt=""/>
+			</figure>
 			<h1>Dicionario Wapichana</h1>
 		</div>
 	</header>
