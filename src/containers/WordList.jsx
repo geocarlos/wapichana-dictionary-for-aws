7238b64b4367aa092fa597592c2f2a936a5f9968
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-06-27 14:50:51 -0300

    Filter Nh properly

--- src/containers/WordList.jsx
+++ src/containers/WordList.jsx
@@ -2,6 +2,10 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/app-context';
 
+const isNotNh = word => {
+	return word.substring(0,2).toLowerCase() !== 'nh'; 
+}
+
 const WordList = ({ letter = 'A'}) => {
 	const { wordList } = useContext(AppContext);
 
@@ -10,7 +14,7 @@ const WordList = ({ letter = 'A'}) => {
 			<h2 id={letter}>{letter}</h2>
 			<ul>
 				{Object.keys(wordList).map((d, i) => {
-					const check = letter.toLowerCase() === d[0].toLowerCase() || letter.toLowerCase() === d.substring(0, 2).toLowerCase();
+					const check = (letter.toLowerCase() === d[0].toLowerCase() && isNotNh(d)) || letter.toLowerCase() === d.substring(0, 2).toLowerCase();
 					return check && <li key={d + i}><Link to={`/${i}/${d}`}>{d}</Link> - {wordList[d].definitions.map(d => d.definition).join('; ')}</li>
 				})}
 			</ul>
