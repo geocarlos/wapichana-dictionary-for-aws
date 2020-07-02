2268e5c53aabc18f8ba51da5971fd3f752ea2955
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-07-01 21:33:11 -0300

    Add audio files

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -7,14 +7,40 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppContext from '../context/app-context';
 
 import wordList from '../data/content.json';
+import _audioList from '../data/audioList.json';
 
 const Main = () => {
 	const [letter, setLetter] = useState('A');
+	const [audioList, setAudioList] = useState(_audioList.map(n => n ? {audio: `https://firebasestorage.googleapis.com/v0/b/wapichana-dictionary.appspot.com/o/audio_${n}.mp3?alt=media`, number: n} : n));
 
+	const moveAudio = i => {
+		const _list = audioList.slice(0);
+		_list.splice(i,0,null);
+		setAudioList(_list);
+	}
+
+	const deleteAudio = i => {
+		const _list = audioList.slice(0);
+		_list.splice(i,1);
+		setAudioList(_list);
+	}
+
+	const save = () => {
+		console.log(JSON.stringify(audioList.map(a => (a || {}).number || null), null, 2))
+		fetch('http://localhost:3003/save', {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+			body: JSON.stringify(audioList.map(a => (a || {}).number || null), null, 2)
+		})
+	}
+ 
 	return (
 		<main>
 			<Header />
-			<AppContext.Provider value={{ wordList }}>
+			<AppContext.Provider value={{ wordList, audioList, moveAudio, deleteAudio }}>
+				<button onClick={save}>Save</button>
 				<Router>
 					<LetterNav setLetter={setLetter} />
 					<Switch>
--- src/containers/WordList.jsx
+++ src/containers/WordList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/app-context';
 
@@ -7,15 +7,47 @@ const isNotNh = word => {
 }
 
 const WordList = ({ letter = 'A'}) => {
-	const { wordList } = useContext(AppContext);
+	const { wordList, audioList, deleteAudio, moveAudio } = useContext(AppContext);
+	
+	const [audio, setAudio] = useState();
+
+	const play = url => {
+		console.log(url)
+		setAudio(new Audio(url));
+	}
+
+	const stop = () => {
+		if (audio) {
+			audio.pause();
+			setAudio(null);
+		}
+	}
+
+	useEffect(() => {
+		if (audio) {
+			audio.play();
+		}
+	}, [audio])
+
 
 	return (
 		<section className="wapi">
 			<h2 id={letter}>{letter}</h2>
 			<ul>
-				{Object.keys(wordList).map((d, i) => {
+				{Object.keys(wordList).filter(w => wordList[w] !== null).map((d, i) => {
 					const check = (letter.toLowerCase() === d[0].toLowerCase() && isNotNh(d)) || letter.toLowerCase() === d.substring(0, 2).toLowerCase();
-					return check && <li key={d + i}><Link to={`/${i}/${d}`}>{d}</Link> - {wordList[d].definitions.map(d => d.definition).join('; ')}</li>
+					return check && wordList[d] && <li key={d + i}>
+						<Link to={`/${i}/${d}`}>{d}</Link> - {wordList[d].definitions.map(d => d.definition).join('; ')}
+						<div>
+							{audioList[i] && (<>
+							<button onClick={() => play(audioList[i].audio)}>Play</button>
+							<button onClick={() => stop()}>Stop</button>
+							<button onClick={() => moveAudio(i)}>Move</button>
+							<button onClick={() => deleteAudio(i)}>Remove</button>
+							<span> Audio Number: {audioList[i].number}</span>
+							</>)}
+						</div>
+						</li>
 				})}
 			</ul>
 		</section>
