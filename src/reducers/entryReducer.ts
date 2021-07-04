2db61bf86d8783c64a95a0e14231bdb7eecac69c
Geocarlos <geowildcat@gmail.com>
2021-07-03 22:13:30 -0300

    Ignore case when sorting

--- src/reducers/entryReducer.ts
+++ src/reducers/entryReducer.ts
@@ -7,8 +7,8 @@ import initialState from '../store/initialState';
 const entries: Reducer<Array<Entry[]>, ActionTypes> = (state = initialState.entries, action: ActionTypes): Array<Entry[]> => {
     switch (action.type) {
         case ActionTypeKeys.FETCH_ENTRIES_RESOLVED:
-            const words = action.payload.data.sort((a: Entry[], b: Entry[]) => a[0].entry_id > b[0].entry_id ? 1 : a[0].entry_id < b[0].entry_id ? -1 : 0);
-            return words.map((w: any) => w.sort((a: Entry, b: Entry) => a.entry_id > b.entry_id ? 1 : a.entry_id < b.entry_id ? -1 : 0));
+            const words = action.payload.data.sort((a: Entry[], b: Entry[]) => a[0].entry_id.toLowerCase() > b[0].entry_id.toLowerCase() ? 1 : a[0].entry_id.toLowerCase() < b[0].entry_id.toLowerCase() ? -1 : 0);
+            return words.map((w: any) => w.sort((a: Entry, b: Entry) => a.entry_id.toLowerCase() > b.entry_id.toLowerCase() ? 1 : a.entry_id.toLowerCase() < b.entry_id.toLowerCase() ? -1 : 0));
         case ActionTypeKeys.CREATE_ENTRY_RESOLVED:
             const newEntry = action.payload.data;
             const index = state.map(e => {
@@ -19,7 +19,7 @@ const entries: Reducer<Array<Entry[]>, ActionTypes> = (state = initialState.entr
                 newState[index] = [newEntry];
             } else {
                 newState.push([newEntry]);
-                newState.sort((a: Entry[], b: Entry[]) => a[0].entry_id > b[0].entry_id ? 1 : a[0].entry_id < b[0].entry_id ? -1 : 0);
+                newState.sort((a: Entry[], b: Entry[]) => a[0].entry_id.toLowerCase() > b[0].entry_id.toLowerCase() ? 1 : a[0].entry_id.toLowerCase() < b[0].entry_id.toLowerCase() ? -1 : 0);
             }
             return newState;
 
